994a7cb82b824f68625aaed9dab46092f0432bc6
ShevaWork <101905253+ShevaWork@users.noreply.github.com>
2024-02-27 12:52:17 +0200

    Update autorefresh_work.js

--- autorefresh_work.js
+++ autorefresh_work.js
@@ -6,101 +6,125 @@
 // @version     1.0
 // ==/UserScript==
 
-const name_zayavka = 'mbaf55d';
-const time_refresh = 750;
 
 (function () {
-      let value = localStorage.getItem("refresherator");
-      checkLocalStore()
-      window.addEventListener('load', build)
-      function build(){
-        if(value === "start"){
-          if(checkZayavka){
-             var interval = setInterval(checkElement, time_refresh);
-          }else{
-            document.addEventListener('DOMContentLoad', checkZayavka)
-          }
+    'use strict';
+    let timeInterval;//const_interval
+    let time = 750;
+    let text = '\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\tОрганізаціявіддаленого робочогомісця (РМ)\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t'
+    let mouseUp = new MouseEvent('mouseup', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+    let classname = 'mbaf55d';
+
+
+    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('load', main);
+
+
+    function main() {
+        const autoClickerStatus = localStorage.getItem('HD_autoClicker');
+        if (autoClickerStatus == 'work') {
+            console.log('HD_autoClicker work');
+            timeInterval = setInterval(work, time);
+        } else {
+            clearInterval(timeInterval);
+            console.log('HD_autoClicker nowork');
         }
-      }
+    }
+
+    function work() {
+        if (document.querySelector('[data-appid="activity"]')) {
+            clickMyZayavka();
+        }
+        if (document.querySelector('[data-appid="startcntr"]')) {
+            perevirka();
+        }
+    }
+
+    function clickMyZayavka() {
+        const woElement = document.querySelector('[id="m43f6b5e4_hyperlink_0-lb"]');
+        const incElement = document.querySelector('[id="mb4bffc9f_hyperlink_0-lb"]');
+
+        if (woElement) {
+            woElement.parentElement.style.backgroundColor = '#77008040';
+            woElement.dispatchEvent(mouseUp);
+            clearInterval(timeInterval);
+            console.log('wo');
+        }
+
+        if (incElement) {
+            incElement.parentElement.style.backgroundColor = '#77008040';
+            incElement.dispatchEvent(mouseUp);
+            clearInterval(timeInterval);
+            console.log('inc');
+        }
+    }
+
+    function perevirka() {
+        const elements = document.querySelectorAll(`[headers="${classname}-content_0"]`);
+
+        if (elements.length > 1 && elements[2].textContent === text) {
+            console.log('HD_autoClicker nowork');
+            clearInterval(timeInterval);
+        } else {
+            clickZayavka();
+        }
+    }
+
+    function clickZayavka() {
+        const zayavkaElements = document.querySelectorAll(`[headers="${classname}-content_0"]`);
+        const refLinkElement = document.getElementById(`${classname}-content_reflink`);
+
+        if (zayavkaElements.length > 1) {
+            const zayavkaPreview = zayavkaElements[1];
+            document.getElementById('psuedoForm').style.backgroundColor = 'red';
+            zayavkaPreview.parentElement.style.backgroundColor = "#77008040";
+            zayavkaPreview.firstElementChild.click();
+            clearInterval(timeInterval);
+            console.log('click');
+        } else if (refLinkElement && zayavkaElements.length <= 1) {
+            const color_refresh = document.getElementById(`${classname}-hb`);
+            color_refresh.style.backgroundColor = getRandomColor();
+            color_refresh.style.borderRadius = "10px";
+            refLinkElement.click();
+            console.log('refresh');
+        } else {
+            console.log('No Work');
+        }
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === 'NumpadSubtract') {
+            const autoClickerStatus = localStorage.getItem('HD_autoClicker');
+            if (autoClickerStatus === 'work') {
+                localStorage.removeItem('HD_autoClicker');
+                clearInterval(timeInterval);
+                console.log('remove HD_autoClicker');
+            } else {
+                localStorage.setItem("HD_autoClicker", "work");
+                timeInterval = setInterval(work, time);
+                console.log('add HD_autoClicker');
+            }
+        }
+
+
+
+    }
+
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+
 })();
 
-function checkZayavka() {
-  if (document.getElementById('m43f6b5e4_hyperlink_0-lb')) {
-      document.getElementById('m43f6b5e4_hyperlink_0-lb').dispatchEvent(new MouseEvent('mouseup', {
-      bubbles: true,
-      cancelable: true,
-      view: window
-      }));
-  } else if(document.getElementById('mb4bffc9f_hyperlink_0-lb')) {
-      document.getElementById('mb4bffc9f_hyperlink_0-lb').dispatchEvent(new MouseEvent('mouseup', {
-      bubbles: true,
-      cancelable: true,
-      view: window
-      }));
-  }else{
-      return true
-  }
-}
 
-function checkElement() {
-    let zayavkaElements = document.querySelectorAll('td[headers="' + name_zayavka + '-content_0"]');
-    let refreshArray = document.getElementById(name_zayavka + '-content_reflink')
-    let colorElement = document.getElementById(name_zayavka + '-hb')
-
-    // Перевірка, чи знайдено елементи
-    if (zayavkaElements.length > 1) {
-        clearInterval(interval); // Зупиняємо інтервал
-        let new_zayavka = zayavkaElements[1].firstElementChild;
-        zayavkaElements[1].parentElement.style.backgroundColor = "#77008040";
-        document.getElementById('psuedoForm').style.backgroundColor = 'red';
-        new_zayavka.click();
-    } else {
-        if (refreshArray) {
-            refreshArray.click(); // Натискання на елемент refreshArray
-            const newColor = getRandomColor();
-            colorElement.style.backgroundColor = newColor;
-            colorElement.style.borderRadius = "10px";
-        } else {
-            console.log('Елементи не знайдено та не знайдений елемент refreshArray');
-        }
-    }
-}
-
-function checkLocalStore() {
-    document.addEventListener("keydown", function (event) {
-        // Перевірка натискання NumpadSubtract
-        let value = localStorage.getItem("refresherator");
-        if (event.code === "NumpadSubtract") {
-            if (value === "start") {
-                localStorage.removeItem('refresherator');
-                clearInterval(interval)
-                console.log('clearInterval')
-            }
-            else {
-                localStorage.setItem('refresherator', 'start');
-                var interval = setInterval(checkElement, time_refresh);
-            }
-        }
-    });
-}
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-
-//текст із заявки
-// var textElement = document.querySelectorAll('td[headers="mdbef32ea-content_0"]')[2].firstElementChild.firstElementChild;
-
-// // Отримуємо всі тексти з дочірніх елементів
-// var textNodes = Array.from(textElement.childNodes)
-//   .filter(node => node.nodeType === Node.TEXT_NODE)
-//   .map(node => node.textContent.trim())
-//   .join('');
-
-// console.log(textNodes);
